a81c53a4f22b399b6f7c6fc75eb8475a9bab854c
Faruk <faruk.seremet@ministryofprogramming.com>
2022-08-31 11:56:33 +0200

    Prettier fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
   root: true,
-  extends: ["react-app", "plugin:react-hooks/recommended"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "import"],
+  extends: ['react-app', 'plugin:react-hooks/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import'],
   overrides: [
     {
-      files: ["*.ts", "*.tsx"],
+      files: ['*.ts', '*.tsx'],
       rules: {
-        "@typescript-eslint/no-shadow": ["error"],
-        "no-shadow": "off",
-        "no-undef": "off",
-        "no-undef-init": "off",
-        "import/order": [
-          "error",
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+        'no-undef-init': 'off',
+        'import/order': [
+          'error',
           {
-            "newlines-between": "always",
+            'newlines-between': 'always',
           },
         ],
       },
